90633b757d58cbb3e8cdb31fa247171bc6f84a08
SEH00N <06psh0501@gmail.com>
2023-09-03 22:03:15 +0900

    async echo server

---
+++ ServerLecture/AsyncEchoServer/Server/Listener.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Listener
+    {
+        private Socket listenSocket = null; // 리슨소켓
+        private List<Socket> clientSockets = new List<Socket>(); // 리슨소켓과 연결 되어있는 소켓 리스트
+
+        private Action<Socket, string> onMessageReceivedEvent; // 메세지가 받아졌을 때 실행시킬 이벤트
+
+        private object locker = new object();
+
+        public Listener(IPEndPoint endPoint, int backlog, Action<Socket, string> onMessageReceived)
+        {
+            // 소켓 생성 & 바인딩
+            listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(backlog);
+
+            onMessageReceivedEvent += onMessageReceived; // 콜백 구독
+        }
+
+        public void Broadcast(string message)
+        {
+            byte[] sendBytes = Encoding.UTF8.GetBytes(message); // 메세지 변환
+
+            // 클라이언트들에게 보낼 데이터
+            SocketAsyncEventArgs sendArgs = new SocketAsyncEventArgs();
+            sendArgs.SetBuffer(sendBytes);
+
+            lock(locker) // clientSockets 라킹
+                clientSockets.ForEach(socket => socket.SendAsync(sendArgs)); // 모든 클라이언트들에게 메세지 전송
+        }
+
+        #region Accept
+
+        public void StartAccept()
+        {
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs(); // 소켓 비동기 작업에 사용되는 이벤트 인자
+            args.Completed += OnAcceptCompleted; // Completed 콜백에 OnAcceptCompleted 구독
+
+            Accept(args); // 실제로 받는 소켓 작업은 Accept 메소드에서 처리
+        }
+
+        private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if(args.SocketError == SocketError.Success) // 소켓이 성공적으로 받아졌다면
+            {
+                Socket clientSocket = args.AcceptSocket;
+                StartReceive(clientSocket); // 메세지 수신 시작
+                lock(locker) // clientSockets 라킹
+                    clientSockets.Add(clientSocket); // 소켓 리스트에 추가
+            }
+            else // 소켓 받아들이기에 실패했다면
+                Console.WriteLine(args.SocketError); // 소켓에러 출력
+
+            Accept(args); // 다시 받기 시작
+        }
+
+        private void Accept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null; // 이벤트 매개변수를 재사용하기 때문에 소켓을 받는 데에 사용되는 AcceptSocket 필드 초기화
+
+            bool pending = listenSocket.AcceptAsync(args); // 비동기로 소켓을 받는 함수. 즉시 처리되었다면 false, 작업이 보류중이면 true 반환
+            if (pending == false) // 만약 즉시 처리가 되었다면
+                OnAcceptCompleted(null, args); // 즉시 처리가 되었다면 Completed 콜백이 실행되지 않기 때문에 직접 핸들링
+        }
+
+        public void Kick(Socket socket)
+        {
+            // 소켓 내보내기
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+
+            lock(locker) // clientSockets 라킹
+                clientSockets.Remove(socket); // clientSockets에서 내보낸 소켓 지우기
+        }
+
+        #endregion
+
+        #region Receive
+
+        private void StartReceive(Socket socket)
+        {
+            SocketAsyncEventArgs receiveArgs = new SocketAsyncEventArgs(); // 메세지를 받을 때 사용하는 인자
+            receiveArgs.Completed += OnReceiveCompleted; // Receive가 끝났을 때 실행되는 Completed 콜백에 OnReceiveCompleted 구독
+            receiveArgs.UserToken = socket; // 토큰으로 클라이언트 소켓 넘기기
+
+            Receive(socket, receiveArgs); // 실질적인 메세지 수신
+        }
+
+        private void Receive(Socket socket, SocketAsyncEventArgs args)
+        {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024], 0, 1024); // 메세지를 받을 버퍼
+            args.SetBuffer(buffer.Array, buffer.Offset, buffer.Count); // 인자에 버퍼 세팅
+
+            bool pending = socket.ReceiveAsync(args); // 비동기로 메세지를 받는 함수. 즉시 처리되었다면 false, 작업이 보류중이면 true 반환
+            if (pending == false) // 반약 동기적으로 처리되었다면
+                OnReceiveCompleted(null, args); // 즉시 처리가 되었다면 Completed 콜벡이 실행되지 않기 때문에 직접 핸들링
+        }
+
+        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket; // 토큰으로 넘어온 소켓 받기
+            if (args.SocketError == SocketError.Success && args.BytesTransferred > 0) // 만약 받은 메세지의 길이가 0보다 크고 성공적으로 통신이 되었다면
+            {
+                string receivedMessage = Encoding.UTF8.GetString(args.Buffer, 0, args.BytesTransferred); // 받은 메세지 변환
+                lock(locker) // onMessageReceivedEvent 라킹
+                    onMessageReceivedEvent?.Invoke(socket, receivedMessage); // onMessageReceivedEvent 발행
+
+                Receive(socket, args); // 다시 메세지 수신 시작
+            }
+            else
+                Kick(socket); // 통신에 실패했다면 소켓 내보내기
+        }
+
+        #endregion
+    }
+}
---
+++ ServerLecture/AsyncEchoServer/Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static Listener listener;
+
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName(); // 해당 로컬 컴퓨터의 DNS 호스트 이름을 불러옴
+            IPHostEntry ipHost = Dns.GetHostEntry(host); // DNS를 바탕으로 IP 주소 정보 객체 생성
+            IPAddress ipAddress = ipHost.AddressList[1]; // 같은 IP리스트에서 두번째 IP 주소를 뽑음
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 8081); // 8081 포트로 소켓이 열릴 종단점 생성
+
+            listener = new Listener(endPoint, 10, OnMessageReceived); // 리스너 생성
+            listener.StartAccept(); // 소켓 받아들이기 시작
+        }
+
+        private static void OnMessageReceived(Socket socket, string msg)
+        {
+            IPEndPoint clientEndPoint = (socket.RemoteEndPoint as IPEndPoint); // 소켓 종단점 받기
+
+            string message = $"{clientEndPoint.Address} : {msg}"; // 출력할 메세지 생성 {보낸 IP : 메세지} 형태
+            Console.WriteLine(message); // 메세지 출력
+            listener.Broadcast(message); // 받은 메세지를 모든 클라이언트들에게 전송
+
+            if (msg.IndexOf("exit") > -1) // 만약 exit 라는 메세지가 포함되어있었으면
+                listener.Kick(socket); // 해당 소켓 내보내기
+        }
+    }
+}
